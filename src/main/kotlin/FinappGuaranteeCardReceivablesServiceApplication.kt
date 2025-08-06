382d9741c9210a5d653134fdd3bfc67ae3677bb1
mvmaraujo <mv.maycon.araujo.santos@gmail.com>
2025-08-06 11:35:38 -0400

    feat: improve Spring Boot application configuration
    - Add @EntityScan annotation to scan domain entities
    - Add @EnableJpaRepositories annotation to enable JPA repositories
    - Improve application structure and entity/repository discovery
    - Better organization of Spring Boot components

--- src/main/kotlin/FinappGuaranteeCardReceivablesServiceApplication.kt
+++ src/main/kotlin/FinappGuaranteeCardReceivablesServiceApplication.kt
@@ -1,9 +1,14 @@
 package com.finapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@SpringBootApplication class FinappGuaranteeCardReceivablesServiceApplication
+@SpringBootApplication
+@EntityScan("com.finapp.domain.*")
+@EnableJpaRepositories("com.finapp.resources.repositories")
+class FinappGuaranteeCardReceivablesServiceApplication
 
 fun main(args: Array<String>) {
   runApplication<FinappGuaranteeCardReceivablesServiceApplication>(*args)
